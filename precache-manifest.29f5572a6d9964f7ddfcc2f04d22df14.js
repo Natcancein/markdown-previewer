846d33bcdf86011a080cb37d49a020c3673c0030
Natcancein <nataliaconstanz@gmail.com>
2019-04-17 12:01:46 -0400

    Updates

---
+++ precache-manifest.29f5572a6d9964f7ddfcc2f04d22df14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ac31481741a1aafa92f",
+    "url": "/markdown-previewer/static/js/runtime~main.2b49566a.js"
+  },
+  {
+    "revision": "d6415a56d6d3bb719aba",
+    "url": "/markdown-previewer/static/js/main.da889818.chunk.js"
+  },
+  {
+    "revision": "bc943669062486e9ca91",
+    "url": "/markdown-previewer/static/js/2.93f8f3a0.chunk.js"
+  },
+  {
+    "revision": "d6415a56d6d3bb719aba",
+    "url": "/markdown-previewer/static/css/main.c8b23a77.chunk.css"
+  },
+  {
+    "revision": "7b95383042fe9ec7722786fc6c870f47",
+    "url": "/markdown-previewer/index.html"
+  }
+];
